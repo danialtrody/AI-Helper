9d1017569fef91080472549e032195b90c989f9d
danial <trody2001@gmail.com>
2025-12-26 22:04:16 +0200

    CSS ADJUST

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# GEMINI_API_KEY=AIzaSyBCN2XZ2ZPbB8q2LKqaPd5sAgy_1w7i0-A
-# GEMINI_API_KEY=AIzaSyBWkFuFmLw41ybBK6qEfSzmco-fYETM9Ds
+
 
 load_dotenv()
 

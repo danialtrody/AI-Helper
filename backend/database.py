5a3b2804afd178e0003126f7f4dd1640aa82f86f
danial <trody2001@gmail.com>
2025-12-26 19:08:53 +0200

    connect to sqlite3

--- backend/database.py
+++ backend/database.py
@@ -5,9 +5,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+#
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///chat.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
+
 
 
 SessionLocal = sessionmaker(
